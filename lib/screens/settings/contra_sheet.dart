d9dfd3c9c92e001b72d1cdbf76d7a836ddf133a0
Imanuel Pundoko <ilomon10@gmail.com>
2022-08-09 20:28:15 +0800

    Fix contra sheet wiget

--- lib/screens/settings/contra_sheet.dart
+++ lib/screens/settings/contra_sheet.dart
@@ -1,6 +1,7 @@
 import 'package:borong/utilities/contra/colors.dart';
 import 'package:borong/widgets/contra/contra_text.dart';
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class ContraSheet extends StatefulWidget {
   final Widget child;
@@ -30,20 +31,15 @@ class ContraSheet extends StatefulWidget {
     required Widget child,
   }) async {
     showModalBottomSheet(
-      backgroundColor: Colors.transparent,
       context: context,
       isScrollControlled: true,
+      backgroundColor: Colors.transparent,
       builder: (context) => ContraSheet(
         onInitState: onInitState,
         onDispose: onDispose,
         title: title,
         action: action,
-        child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 24.0,
-              vertical: 12.0,
-            ),
-            child: child),
+        child: child,
       ),
     );
   }
@@ -65,51 +61,66 @@ class _ContraSheetState extends State<ContraSheet> {
   @override
   Widget build(BuildContext context) {
     double keyboardPadding = MediaQuery.of(context).viewInsets.bottom;
-    return Wrap(
-      children: <Widget>[
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(16),
-              topRight: Radius.circular(16),
-            ),
-            border: Border.all(color: ContraColors.woodSmoke, width: 2),
-          ),
-          child: (Column(
-            children: <Widget>[
-              Container(
-                decoration: const BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(color: ContraColors.woodSmoke, width: 2),
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 24, right: 12),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        flex: 2,
-                        child: ContraText(
-                          alignment: Alignment.centerLeft,
-                          text: widget.title,
-                          size: 24,
-                        ),
-                      ),
-                      if (widget.action != null)
-                        Flexible(flex: 0, child: widget.action!)
-                    ],
-                  ),
-                ),
-              ),
-              SingleChildScrollView(
-                child: widget.child,
-              ),
-              SizedBox(height: keyboardPadding > 0 ? keyboardPadding : 24),
-            ],
-          )),
+    double statusbarPadding = MediaQueryData.fromWindow(window).padding.top;
+    return GestureDetector(
+      onTap: () => Navigator.pop(context),
+      child: Container(
+        margin: EdgeInsets.only(
+          top: statusbarPadding,
+          bottom: keyboardPadding,
         ),
-      ],
+        color: const Color.fromRGBO(0, 0, 0, 0.001),
+        child: GestureDetector(
+          onTap: (() {}),
+          child: DraggableScrollableSheet(builder: (_, controller) {
+            return Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(16),
+                  topRight: Radius.circular(16),
+                ),
+                border: Border.all(color: ContraColors.woodSmoke, width: 2),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        bottom:
+                            BorderSide(color: ContraColors.woodSmoke, width: 2),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 24, right: 12),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            flex: 2,
+                            child: ContraText(
+                              alignment: Alignment.centerLeft,
+                              text: widget.title,
+                              size: 24,
+                            ),
+                          ),
+                          if (widget.action != null)
+                            Flexible(flex: 0, child: widget.action!)
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      controller: controller,
+                      child: widget.child,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }),
+        ),
+      ),
     );
   }
 }
